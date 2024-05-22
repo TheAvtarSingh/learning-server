ab826d4903d0e0e77e43391be89f435bb3b5ee34
AVTAR SINGH <avtarsingh@AVTARs-MacBook-Air.local>
2024-05-22 22:48:58 +0530

    key

--- routes/payment-routes/keys.js
+++ routes/payment-routes/keys.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get("/getKey",async(req,res)=>{
     res.send({KeyId:process.env.RAZORPAY_KEY_ID}).status(200);
 })
+router.get("/openAiKey",async(req,res)=>{
+    res.send({KeyId:process.env.OPEN_AI_KEY}).status(200);
+})
 
 module.exports = router;
